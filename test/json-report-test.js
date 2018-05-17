2578296c51c620180ebd979f73c7ff1f28158aa3
Marc Dix <marcdix@marcdix.de>
2018-05-17 08:00:03 +0200

    refactor(detail): refactoring json report test

--- test/json-report-test.js
+++ test/json-report-test.js
@@ -6,8 +6,17 @@ const Keyfob = require('keyfob')
 
 const fixtures = Keyfob.load({ path: 'test/fixtures', fn: require })
 
-tap.test('it generates a json report', function (t) {
-  return Report(fixtures['no-vulns'], {reporter: 'json'}).then((report) => {
-    t.match(report.exitCode, 0)
+tap.test('it generates a json report with zero for every severity', function (t) {
+  return Report(fixtures['no-vulns'], {reporter: 'json'}).then((reportRaw) => {
+    const exitCode = reportRaw.exitCode
+    const vulnerabilities = JSON.parse(reportRaw.report).metadata.vulnerabilities
+
+    t.equal(exitCode, 0)
+
+    t.equal(vulnerabilities.info, 0)
+    t.equal(vulnerabilities.low, 0)
+    t.equal(vulnerabilities.moderate, 0)
+    t.equal(vulnerabilities.high, 0)
+    t.equal(vulnerabilities.critical, 0)
   })
 })
