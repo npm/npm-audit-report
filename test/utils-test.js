950fc23195039ff2ed04ae12ac3caadd03d4f11e
Marc Dix <marcdix@marcdix.de>
2018-05-30 07:25:22 +0200

    tests(utils): Adding tests for util function color()

---
+++ test/utils-test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const tap = require('tap')
+const {severityLabel, color} = require('../lib/utils')
+
+tap.test('color does just return the value if colorName and withColor are missing', function (t) {
+  t.equal(color('Low'), 'Low')
+  t.equal(color('something'), 'something')
+  t.done()
+})
+
+tap.test('color returns a formatted string if all params are set', function (t) {
+  t.equal(color('Low', 'brightGreen', true), '\x1b[92mLow\x1b[0m')
+  t.equal(color('High', 'brightRed', true), '\x1b[91mHigh\x1b[0m')
+  t.done()
+})
+
+tap.test('severity does not throw an error if given severity is not defined', function (t) {
+  t.throws(function () { severityLabel('me-no-exist') })
+  t.done()
+})
