afe44c652b5f93022d44a40a06ef302001d84263
isaacs <i@izs.me>
2020-05-01 01:39:21 -0700

    force color support in CI

--- test/colors.js
+++ test/colors.js
@@ -1,5 +1,21 @@
-const colors = require('../lib/colors.js')
 const t = require('tap')
+const requireInject = require('require-inject')
+// force color support when in CI, so tests do the right thing
+const cs = {
+  hasBasic: true,
+  has256: true,
+  has16m: true,
+  level: 3,
+}
+const colors = requireInject('../lib/colors.js', {
+  'chalk': requireInject('chalk', {
+    'supports-color': Object.assign(() => cs, {
+      stdout: cs,
+      stderr: cs,
+    })
+  })
+})
+t.formatSnapshot = ({report, exitCode}) => `${report}\nexitCode=${exitCode}`
 
 t.test('with colors', async t => {
   const c = colors(true)
