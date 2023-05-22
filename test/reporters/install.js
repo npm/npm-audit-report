8f95c46df37563d66140cc2f61da006e8437fa56
Luke Karrys <luke@lukekarrys.com>
2023-05-22 10:03:15 -0700

    chore: fix tests to use chalk@5 as dev dep (#89)

--- test/reporters/install.js
+++ test/reporters/install.js
@@ -1,6 +1,6 @@
 const t = require('tap')
 const fixture = require('../fixtures/index.js')
-const { color, noColor } = require('../fixtures/chalk')
+const chalk = require('../fixtures/chalk')
 const Report = require('../..')
 const install = require('../../lib/reporters/install.js')
 
@@ -9,6 +9,7 @@ t.formatSnapshot = ({ report, exitCode }) => `${report}\nexitCode=${exitCode}`
 for (const f of fixture.files) {
   t.test(f, async t => {
     const data = fixture(f)
+    const { color, noColor } = await chalk()
     t.matchSnapshot({ report: install.summary(data, { chalk: color }) }, 'summary color')
     t.matchSnapshot({ report: install.summary(data, { chalk: noColor }) }, 'summary no color')
     t.matchSnapshot(Report(data, { reporter: 'install', chalk: color }), 'default settings')
